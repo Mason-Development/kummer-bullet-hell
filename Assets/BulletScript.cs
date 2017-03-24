5fc09be0811b0ae6989c6ab4aa68be252553be43
Dalton Craven <20dalton00@gmail.com>
2017-03-23 21:43:13 -0400

    Add Assets

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum bulletTypes { Player, EnemyN}
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class BulletScript : MonoBehaviour {
+    public bulletTypes mType;
+    public float speed;
+    Rigidbody2D rbody;
+
+	void Start ()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate ()
+    {
+        rbody.velocity = Vector2.down * (speed + Time.deltaTime);
+	}
+}
---
+++ Assets/PatternData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternData : MonoBehaviour {
+    public int bulletsInPattern;
+    public Quaternion[] rotations;
+    public float bulletSpeed;
+
+    void SelfDestruct()
+    {
+        Destroy(gameObject);
+    }
+}
