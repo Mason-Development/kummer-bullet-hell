db17c3fed75312af7f5feb43481af2230d9c4863
Yaboiethan <ethanMH2002@gmail.com>
2017-04-25 15:16:13 -0400

    Boss script
    Yaaas

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+	
+	public float speed;
+
+	public Transform Player;
+
+	void Update () 
+	{
+		Vector3 des = new Vector3 (Player.position.x, transform.position.y, transform.position.z);
+		transform.position = Vector3.Lerp(transform.position, des, speed);
+	}
+}
